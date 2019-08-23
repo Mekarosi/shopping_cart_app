62f3ca248ddb18ecea253d2234eb4f6f7bc4497e
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-23 22:43:33 +0100

    Dynamically rendered list of state properties

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
-  newMethod() {
+  getBadgeClasses() {
     let classes = 'badge m-2 badge-';
     classes += this.state.count === 0 ? 'warning' : 'primary';
     return classes;
